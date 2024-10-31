69232af589360285d9cab5961a74507be77fcc3d
morgan-187 <lao0978177213@gmail.com>
2024-11-01 07:55:56 +0800

    增加餐廳時增加庫存0 信縮短間隔

--- src/main/java/com/eatspan/SpanTasty/service/rental/OvertimeRentSendMailService.java
+++ src/main/java/com/eatspan/SpanTasty/service/rental/OvertimeRentSendMailService.java
@@ -40,7 +40,7 @@ public class OvertimeRentSendMailService {
 	
 	
 	// 每天早上8點發送超時信
-	//@Scheduled(fixedRate = 30000)//每半分鐘執行
+	@Scheduled(fixedRate = 5000)//每半分鐘執行
 	public void findOvertimeRents() throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, MessagingException, IOException, TemplateException {
 		List<Rent> overtimeRents = rentService.findByDueDateTomorrow();
 		
